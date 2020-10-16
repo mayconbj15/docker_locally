fb6eff4c092f26eb73082d411493db5e7c88b02c
Maycon Bruno de Jesus <maycon.jesus@bancointer.com.br>
2020-10-16 19:21:19 -0300

    refactor and adding remove container

--- dotnet-docker.py
+++ dotnet-docker.py
@@ -19,6 +19,7 @@ def get_arguments():
     parser.add_argument("-in", "--image-name", dest = "image_name", default = "dotnet-image", help="The name of the image that docker will build")
     parser.add_argument("-p", "--port", dest = "port", default = "8080:8080", help="Publish a container's port(s) to the host")
     parser.add_argument("-pn", "--project-name", dest = "project_name", default = "", required=True, help="The name of project")
+    parser.add_argument("-r", "--remove", dest = "remove", default = "n", help="Remove the container after the execution")
     parser.add_argument("-wf", "--workspace-folder", dest = "workspace_folder", default = "", required=True, help="The path of the workspace folder of the project")
     
     global args
@@ -26,15 +27,42 @@ def get_arguments():
 
     print(args)
 
-def readYamlFile(dir_path, base_command):
-    with open(dir_path+'/values.yaml') as f:
-        j = yaml.load(f, Loader=yaml.FullLoader)
+#dotnet stages functions
+def dotnet_stages(dir_path):
+    if args.dotnet_clean == 'y':
+        dotnet_clean()
+    
+    publish(args.workspace_folder + '/src/' + args.project_name, dir_path)
+    copy_envs(args.workspace_folder, dir_path)
 
-        for x in j["env"]:
-            base_command.append('--env')
-            base_command.append(str(x['name']) + '=' + str(x['value']))        
-        
-    return base_command
+def dotnet_clean():
+    command = ['dotnet', 'clean', get_proj_file()]
+    print(command)
+
+def publish(base_path, dir_path):
+    pubish_command = ['dotnet', 'publish', base_path, '-o', dir_path+'/publish', '-c', 'release']
+    run_command(pubish_command)
+
+def copy_envs(base_path, dir_path):
+    copy_runsh(base_path, dir_path)
+    copy_values(base_path, dir_path)
+
+def copy_runsh(base_path, dir_path):
+    cp_command = ['cp', base_path + '/run.sh', dir_path]
+    run_command(cp_command)
+
+def copy_values(base_path, dir_path):
+    cp_command = ['cp', base_path + '/src/main/kubernetes/dev/values.yaml', dir_path]
+    run_command(cp_command)
+
+def get_proj_file():
+    return args.workspace_folder + '/src/' + args.project_name + '/' + args.project_name + '.csproj'
+
+#docker stages functions
+def docker_stages(dir_path):
+    docker_build(dir_path)
+    docker_run(dir_path)
+    stop_container()
 
 def docker_build(dir_path):
     build_command = ['docker', 'build', '--tag', args.image_name, dir_path+'/.']
@@ -56,21 +84,27 @@ def docker_run(dir_path):
 
     run_command(base_command)
 
-def copy_runsh(base_path, dir_path):
-    cp_command = ['cp', base_path + '/run.sh', dir_path]
-    run_command(cp_command)
+def stop_container():
+    if args.detach == 'n':
+        print('STOPING THE CONTAINER')
+        command_stop = ['docker stop $(docker ps -q -l)']
+        run_command(command_stop, shell=True)
+        if args.remove == 'y':
+            print('REMOVING THE CONTAINER')
+            command_remove = ['docker rm $(docker ps -q -l)']
+            run_command(command_remove, shell=True)
+    else:
+        print('RUNNING CONTAINER IN DETACH MODE\n' + 'Type "docker ps" to see the container info')
 
-def copy_values(base_path, dir_path):
-    cp_command = ['cp', base_path + '/src/main/kubernetes/dev/values.yaml', dir_path]
-    run_command(cp_command)
+def readYamlFile(dir_path, base_command):
+    with open(dir_path+'/values.yaml') as f:
+        j = yaml.load(f, Loader=yaml.FullLoader)
 
-def copy_envs(base_path, dir_path):
-    copy_runsh(base_path, dir_path)
-    copy_values(base_path, dir_path)
-    
-def publish(base_path, dir_path):
-    pubish_command = ['dotnet', 'publish', base_path, '-o', dir_path+'/publish', '-c', 'release']
-    run_command(pubish_command)
+        for x in j["env"]:
+            base_command.append('--env')
+            base_command.append(str(x['name']) + '=' + str(x['value']))        
+        
+    return base_command
 
 def run_command(command, shell=False):
     try:
@@ -81,36 +115,10 @@ def run_command(command, shell=False):
     except:
         print('ERRO AO EXECUTAR COMANDO')
 
+#aws functions
 def get_credentials(dir_path):
     command = ['aws-vault exec ' + args.env + ' -- env | grep --color=never ^AWS_ > ' + dir_path+'/env_file']
     run_command(command, shell=True)
-
-def stop_container():
-    if args.detach == 'n':
-        print('STOPING THE CONTAINER')
-        command = ['docker stop $(docker ps -q)']
-        run_command(command, shell=True)
-    else:
-        print('RUNNING CONTAINER IN DETACH MODE\n' + 'Type "docker ps" to see the container info')
-
-def dotnet_clean():
-    command = ['dotnet', 'clean', get_proj_file()]
-    print(command)
-
-def get_proj_file():
-    return args.workspace_folder + '/src/' + args.project_name + '/' + args.project_name + '.csproj'
-
-def dotnet_stages(dir_path):
-    if args.dotnet_clean == 'y':
-        dotnet_clean()
-    
-    publish(args.workspace_folder + '/src/' + args.project_name, dir_path)
-    copy_envs(args.workspace_folder, dir_path)
-
-def docker_stages(dir_path):
-    docker_build(dir_path)
-    docker_run(dir_path)
-    stop_container()
 
 def main():
     get_arguments()
@@ -124,6 +132,5 @@ def main():
     
     docker_stages(dir_path)
     
-
 if __name__ == "__main__":
     main()
